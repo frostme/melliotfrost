103754a11b6a9fd082adefea328c1844fd101c11
frostme <melliotfrost@gmail.com>
2019-01-15 10:36:29 -0500

    updates

---
+++ public/img/portfolio/Explore Studios _ ClassPass_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "WASHINGTON",
+      'continent': "NA",
+      'country': "US",
+      'region': "DC"
+    },
+    'ip':"98.233.95.74"
+  }]);
+})
+//
+()
+
+;
